2e53c6daad0f9e1e1dcf1b3fb715d719c6b528ef
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-06 14:52:15 -0500

    Coding Challenge Solution

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,2 +1,11 @@
 module.exports = function (string1, string2) {
+  const stringresult = (str) => {
+    return str
+        .replace(/[^\w]/g, '')
+        .toLowerCase()
+        .split('')
+        .sort()
+        .join('')
+}
+return stringresult(string1) === stringresult(string2);
 }
